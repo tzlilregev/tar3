1b9015ebd79f3a4392fe1a1525bd66b0d2617a16
einavdror <34092116+einavdror@users.noreply.github.com>
2017-11-29 13:06:33 +0200

    fhkjdsz

--- WebSite/Default.aspx.cs
+++ WebSite/Default.aspx.cs
@@ -10,5 +10,6 @@ public partial class _Default : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         string i = "elior";
+        string i1 = "einva";
     }
 }
